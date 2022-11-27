05a59950957c4614f43c32831577b09518ec69b9
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-11-27 16:31:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n, x=1, y=1, z=0;
+    cout<< "scrivi il numero n"<<endl;
+    cin>>n;
+    cout<<x<<endl<<y<<endl;
+    for (int i=0; i<=n; i++)
+    {
+       z=x + y;
+       x=y;
+       y=z;
+       cout<< z<<endl;
+    }
+    
+    return 0;
 }
